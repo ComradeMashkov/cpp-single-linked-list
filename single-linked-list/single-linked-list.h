bbdd179fbea2ece7e6b4b7f7b56959d4183646b1
ComradeMashkov <comrademashkov@gmail.com>
2023-07-17 16:53:41 +0300

    Fixed reviewer's remarks

--- single-linked-list/single-linked-list.h
+++ single-linked-list/single-linked-list.h
@@ -71,6 +71,7 @@ class SingleLinkedList {
         // Оператор прединкремента. После его вызова итератор указывает на следующий элемент списка
         // Возвращает ссылку на самого себя
         BasicIterator& operator++() noexcept {
+			assert(node_ != nullptr);
             node_ = node_->next_node;
             return *this;
         }
@@ -85,12 +86,14 @@ class SingleLinkedList {
 
         // Операция разыменования. Возвращает ссылку на текущий элемент
         [[nodiscard]] reference operator*() const noexcept {
+			assert(node_ != nullptr);
             return node_->value;
         }
         
         // Операция доступа к члену класса. Возвращает указатель на текущий элемент списка
         [[nodiscard]] pointer operator->() const noexcept {
-            return (node_ != nullptr) ? &node_->value : nullptr;
+			assert(node_ != nullptr);
+            return &node_->value;
         }
         
     private:
@@ -111,32 +114,22 @@ public:
     
     }
     
-    template <typename IteratorType>
-    void Init(IteratorType range_begin, IteratorType range_end) {
-        Node* node = &head_;
-        for (auto it = range_begin; it != range_end; ++it) {
-            ++size_;
-            node->next_node = new Node(*it, nullptr);
-            node = node->next_node;
-        }
-    }
-    
     SingleLinkedList(std::initializer_list<Type> values) {
         SingleLinkedList tmp;
         tmp.Init(values.begin(), values.end());
-        swap(tmp);
+        this->swap(tmp);
     }
     
     SingleLinkedList(const SingleLinkedList& other) {
         SingleLinkedList tmp;
         tmp.Init(other.begin(), other.end());
-        swap(tmp);
+        this->swap(tmp);
     }
     
     SingleLinkedList& operator=(const SingleLinkedList& rhs) {
         if (this != &rhs) {
             SingleLinkedList tmp(rhs);
-            swap(tmp);
+            this->swap(tmp);
         }
         return *this;
     }
@@ -158,6 +151,7 @@ public:
      * Если при создании элемента будет выброшено исключение, список останется в прежнем состоянии
     */
     Iterator InsertAfter(ConstIterator pos, const Type& value) {
+		assert(pos.node_ != nullptr);
         Node* new_node = new Node(value, pos.node_->next_node);
         pos.node_->next_node = new_node;
         ++size_;
@@ -165,6 +159,7 @@ public:
     }
     
     void PopFront() noexcept {
+		assert(!this->IsEmpty());
         Node* tmp = head_.next_node->next_node;
         delete head_.next_node;
         head_.next_node = tmp;
@@ -176,6 +171,7 @@ public:
      * Возвращает итератор на элемент, следующий за удалённым
     */
     Iterator EraseAfter(ConstIterator pos) noexcept {
+		assert(pos.node_ != nullptr && !this->IsEmpty());
         Node* tmp = pos.node_->next_node->next_node;
         delete pos.node_->next_node;
         pos.node_->next_node = tmp;
@@ -199,7 +195,7 @@ public:
 
     // Сообщает, пустой ли список за время O(1)
     [[nodiscard]] bool IsEmpty() const noexcept {
-        return !size_;
+        return size_ == 0u;
     }
     
     // Возвращает итератор, ссылающийся на первый элемент
@@ -257,6 +253,16 @@ private:
     // Фиктивный узел, используется для вставки "перед первым элементом"
     Node head_;
     size_t size_ = 0u;
+
+	template <typename IteratorType>
+    void Init(IteratorType range_begin, IteratorType range_end) {
+        Node* node = &head_;
+        for (auto it = range_begin; it != range_end; ++it) {
+            ++size_;
+            node->next_node = new Node(*it, nullptr);
+            node = node->next_node;
+        }
+    }
 };
 
 template <typename Type>
@@ -266,7 +272,7 @@ void swap(SingleLinkedList<Type>& lhs, SingleLinkedList<Type>& rhs) noexcept {
 
 template <typename Type>
 bool operator==(const SingleLinkedList<Type>& lhs, const SingleLinkedList<Type>& rhs) {
-    return std::equal(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+	return (lhs.GetSize() == rhs.GetSize()) ? std::equal(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()) : false;
 }
 
 template <typename Type>
